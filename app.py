f5e7ec408a6a10653b9d1e9e6fe7872dfed5759b
yumpyy <yumpyy@github.com>
2023-11-04 21:39:03 +0530

    Temp UI

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 
 app.jinja_env.autoescape = False
 
-
 @app.route("/")
+def mainMenu():
+    return render_template("index.html", articles=atomReader("test.xml") )
+    
+
+@app.route("/articles")
 def articleRead():
-    return render_template("index.html", feed=atomReader("test2.rss") )
+    return render_template("articles.html", articles=atomReader("test.xml") )
 
 app.run(debug=True)
--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import feedparser
 def atomReader(url):
     parser = feedparser.parse(url)
     # print(parser)
+    print(len(parser['entries']))
     # print(parser['entries'][0]['summary'])
-    feed = {
-            'title' : parser['entries'][5]['title'],
-            'published' : parser['entries'][5]['published'],
-            'linkOriginal' : parser['entries'][5]['links'][0]['href'],
-            'preview-image' : parser['entries'][5]['media_content'][0]['url'],
-            'content' : parser['entries'][5]['content'][0]['value']
-    }
+    articles = []
+    for x in range(len(parser['entries'])):
+        feed = {
+                'title' : parser['entries'][x]['title'],
+                'published' : parser['entries'][x]['published'],
+                'linkOriginal' : parser['entries'][x]['links'][0]['href'],
+                # 'preview-image' : parser['entries'][x]['media_content'][0]['url'],
+                'content' : parser['entries'][x]['content'][0]['value']
+        }
+        # print(feed)
+        articles.append(feed)
+    
+    return articles
 
-    return feed
-
-# atomReader("test2.rss")
+atomReader("test2.rss")
