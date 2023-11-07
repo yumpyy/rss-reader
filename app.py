05fcc0e7e60d130bfa43b790bebc577e4004e46a
yumpyy <121102787+yumpyy@users.noreply.github.com>
2023-11-07 20:49:05 +0530

    Clicking on feed-card now redirects to that specific article

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from main import feedFetch
 
 app = Flask(__name__)
@@ -7,11 +7,22 @@ app.jinja_env.autoescape = False
 
 @app.route("/")
 def mainMenu():
-    return render_template("index.html", articles=feedFetch())
+    articlesList = feedFetch()
+    return render_template("index.html", articles=articlesList)
     
 
 @app.route("/articles")
 def articleRead():
-    return render_template("articles.html", articles=feedFetch())
+    articlesList = feedFetch()
+
+    articleRequested = request.args.get("q")
+
+    for feed in articlesList:
+        if articleRequested == feed["title"]:
+            return render_template("articles.html", article=feed)
+        else:
+            continue
+
+    return render_template("404.html")
 
 app.run(debug=True)
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import feedparser
-from datetime import datetime
-from dateutil.parser import parse
 from db import urlsFromDatabase
 
 def feedFetch():
@@ -15,48 +13,45 @@ def feedFetch():
         parser = feedparser.parse(url[0])
         # print(parser)
         for x in range(len(parser['entries'])):
-            try:
-                feed = {
-                        'feed-name' : url[1],
-                        'title' : parser['entries'][x]['title'],
-                        'published' : parser['entries'][x]['published_parsed'],
-                        'linkOriginal' : parser['entries'][x]['links'][0]['href']
-                        }
+            feed = {
+                    'feed-name' : url[1],
+                    'title' : parser['entries'][x]['title'],
+                    'published' : parser['entries'][x]['published_parsed'],
+                    'linkOriginal' : parser['entries'][x]['links'][0]['href']
+                    }
 
-                if 'summary' in feed:
-                    feed['summary'] = parser['entries'][x]['summary']
-                else:
-                    feed['summary'] = ''
+            # if 'summary' in feed:
+            #     feed['summary'] = parser['entries'][x]['summary']
+            # else:
+            #     feed['summary'] = ''
 
-                if 'content' in feed:
-                    feed['content'] = parser['entries'][x]['content'][0]['value']
-                else:
-                    feed['content'] = parser['entries'][x]['summary']
+            if 'content' in feed:
+                feed['content'] = parser['entries'][x]['content'][0]['value']
+            else:
+                feed['content'] = parser['entries'][x]['summary']
 
-                articles.append(feed)
-            except KeyError as e:
-                errorKey = e.args[0]
+            articles.append(feed)
 
-                if errorKey in feed:
-                    feed[errorKey] = feed['summary']
-                else:
-                    feed[errorKey] = ''
-        
+    articles = dateSort(articles)
     return articles
 
 def dateSort(articles):
-    dates = []
+    for article in articles:
+        date = article['published']
+        date = list(date)
+        # date = f'{date[0]}/{date[1]}/{date[2]} {date[3]}:{date[4]}'
+        # article['published'] = date 
+
+    articles.sort(key=lambda article: (article['published'][0], article['published'][1], article['published'][2], article['published'][3], article['published'][4]), reverse=True)
+
     for article in articles:
         date = article['published']
         date = list(date)
         date = f'{date[0]}/{date[1]}/{date[2]} {date[3]}:{date[4]}'
-        dates.append(date)
+        article['published'] = date 
 
-        # date.sort(key=lambda date: datetime.strptime(date, '%Y/%m/%d %H:%M'))
-    dates.sort(key=lambda date: datetime.strptime(date, '%Y/%m/%d %H:%M'), reverse=True)
-    print(dates)
-    # Append the sorted dates to the original list
-    articles.sort(key=lambda article: dates[article['published']])
+    # print(articles)
+    return articles
     
-# feedFetch()
-dateSort(feedFetch())
+feedFetch()
+# dateSort(feedFetch())
