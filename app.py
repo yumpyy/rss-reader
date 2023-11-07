e65190aed72405d1c5bc657f6f241b29f0f585e7
yumpyy <121102787+yumpyy@users.noreply.github.com>
2023-11-07 08:38:46 +0530

    MySql for storing fixed

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-
 from flask import Flask, render_template
-from main import atomReader
+from main import feedFetch
 
 app = Flask(__name__)
 
@@ -8,11 +7,11 @@ app.jinja_env.autoescape = False
 
 @app.route("/")
 def mainMenu():
-    return render_template("index.html", articles=atomReader("test.xml") )
+    return render_template("index.html", articles=feedFetch())
     
 
 @app.route("/articles")
 def articleRead():
-    return render_template("articles.html", articles=atomReader("test.xml") )
+    return render_template("articles.html", articles=feedFetch())
 
 app.run(debug=True)
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import feedparser
+from db import urlsFromDatabase
 
-def atomReader(url):
-    parser = feedparser.parse(url)
-    # print(parser)
-    print(len(parser['entries']))
-    # print(parser['entries'][0]['summary'])
+def feedFetch():
+    urls = urlsFromDatabase()
+    print(urls)
     articles = []
-    for x in range(len(parser['entries'])):
-        feed = {
-                'title' : parser['entries'][x]['title'],
-                'published' : parser['entries'][x]['published'],
-                'linkOriginal' : parser['entries'][x]['links'][0]['href'],
-                # 'preview-image' : parser['entries'][x]['media_content'][0]['url'],
-                'content' : parser['entries'][x]['content'][0]['value']
-        }
-        # print(feed)
-        articles.append(feed)
-    
+
+    for url in urls:
+        print(url)
+        parser = feedparser.parse(url)
+
+        for x in range(len(parser['entries'])):
+            feed = {
+                    'title' : parser['entries'][x]['title'],
+                    'published' : parser['entries'][x]['published'],
+                    'link-original' : parser['entries'][x]['links'][0]['href'],
+                    'content' : parser['entries'][x]['content'][0]['value']
+            }
+            articles.append(feed)
+        
     return articles
 
-atomReader("test2.rss")
+feedFetch()
